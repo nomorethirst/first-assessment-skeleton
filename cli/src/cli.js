a28c9318da59030dfa15f6b5a17d41f6b4fb9209
nomorethirst <5522646+nomorethirst@users.noreply.github.com>
2017-11-01 11:44:58 -0500

    Did npm install and implemented src/cli.js host/port args. Tested working.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -7,22 +7,34 @@ export const cli = vorpal()
 
 let username
 let server
+let host
+let port
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username>')
+  .mode('connect <username> [host] [port]')
   .delimiter(cli.chalk['green']('connected>'))
   .init(function (args, callback) {
     username = args.username
-    server = connect({ host: 'localhost', port: 8080 }, () => {
+    host = args.host ? args.host : 'localhost'
+    port = args.port ? args.port : 8080
+    server = connect({ host, port }, () => {
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
     })
 
     server.on('data', (buffer) => {
       this.log(Message.fromJSON(buffer).toString())
+    })
+
+    server.on('error', (error) => {
+      if (error.code === 'ECONNREFUSED') {
+        this.log(`Connection refused at ${error.address}:${error.port}. Try a different host/port.`)
+      } else {
+        this.log(error)
+      }
     })
 
     server.on('end', () => {
